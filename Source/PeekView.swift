03022315be4e59eee6d096d549145d095fe9ce95
China <chg@budist.ru>
2017-03-22 12:09:11 +0200

    sync

--- Source/PeekView.swift
+++ Source/PeekView.swift
@@ -41,12 +41,17 @@ public struct PeekViewAction {
     var arrowImageView: UIImageView?
     
     init() {
-        super.init()
+        super.init(frame: CGRect.zero)
         print("PeekView init")
     }
-    init(frame: CGRect) {
+    override init(frame: CGRect) {
         super.init(frame:frame)
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     deinit {
         print("PeekView deinit")
     }
